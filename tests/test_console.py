6f10ec46a611143093ceecc8982b0eefc5ea27c2
Matrix30 <Matrixboy30@gmail.com>
2023-11-12 16:17:46 +0100

    start console test

--- tests/test_console.py
+++ tests/test_console.py
@@ -3,10 +3,23 @@
 Test module for console intepreter
 """
 
+import imp
 import unittest
+from unittest.mock import patch
+from console import HBNBCommand
+from io import StringIO
 
 
 class TestConsole(unittest.TestCase):
     """Test class for console"""
 
-    pass
+    def test_help_quit(self):
+        """Tests the help command."""
+        with patch("sys.stdout", new=StringIO()) as f:
+            HBNBCommand().onecmd("help quit")
+        s = "Quit command to exit the program\n"
+        self.assertEqual(s, f.getvalue())
+
+
+if __name__ == "__main__":
+    unittest.main()

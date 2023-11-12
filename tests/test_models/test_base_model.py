efcc9a5276160059012f632be3420afae083e33c
Matrix30 <Matrixboy30@gmail.com>
2023-11-12 04:03:41 +0100

    dam

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,9 +1,61 @@
 #!/usr/bin/python3
-"""Modules"""
+"""Test module for base model"""
 import unittest
-from ...models.base_model import BaseModel
+from models.base_model import BaseModel
+import datetime
+from io import StringIO
+from unittest.mock import patch
 
 
 class TestBaseModel(unittest.TestCase):
-    """Tests begins..."""
-    pass
+    """Test class for base model"""
+
+    def test_id(self):
+        """Test cases for public instance id attribute"""
+        # assert the id is of str type
+        self.assertIsInstance(BaseModel().id, str)
+
+        # assert two objects won't have same id
+        self.assertNotEqual(BaseModel().id, BaseModel().id)
+
+        # test if when an object is created both created_at and update_at are
+        # not more than 2 secs apart
+        base = BaseModel()
+        self.assertAlmostEqual(
+            base.created_at, base.updated_at, delta=datetime.timedelta(
+                seconds=2)
+        )
+
+    def test__str__(self):
+        """Test __str__ return value"""
+        base = BaseModel()
+        # testing if __str__ prints correctly
+        res = "[{:s}] ({:s}) {}\n".format(
+            base.__class__.__name__, base.id, base.__dict__
+        )
+        with patch("sys.stdout", new=StringIO()) as str_out:
+            print(base)
+            self.assertEqual(str_out.getvalue(), res)
+
+    def test_to_dict(self):
+        """Test for to_dic method"""
+        base = BaseModel()
+        dic = base.to_dict()
+        # assert method returns dictionary
+        self.assertIsInstance(dic, dict)
+        # assert creat_at and update_at are all strings
+        self.assertIsInstance(dic["created_at"], str)
+        self.assertIsInstance(dic["updated_at"], str)
+
+        # assert the dic has a key called __class__  having the class name
+        self.assertEqual(dic["__class__"], base.__class__.__name__)
+
+        # testing if __class__ key exist in dic
+
+    def test_create_baseModel_from_dict(self):
+        """Testing creation of base model from dict datastructure"""
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()

710971f3605dd474cbd125bf05919f60e79df69b
Matrix30 <Matrixboy30@gmail.com>
2023-11-12 04:19:04 +0100

    dam

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """Test module for base model"""
 import unittest
-from models.base_model import BaseModel
 import datetime
+from models.base_model import BaseModel
 from io import StringIO
 from unittest.mock import patch
 

5c845d25f156bd9ad8819364707758a2fb51677b
Matrix30 <Matrixboy30@gmail.com>
2023-11-12 04:34:39 +0100

    another fix

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -2,9 +2,9 @@
 """
 Test module for amenity model
 """
-
 import unittest
 from models.amenity import Amenity
+
 
 class TestAmenity(unittest.TestCase):
     """Test class for amenity"""
--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -2,8 +2,8 @@
 """
 Test module for city model
 """
-
 import unittest
+from models.city import City
 
 
 class TestCity(unittest.TestCase):

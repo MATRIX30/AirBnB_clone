0c29ae4a796a40f20cc935cfea2e98adb6333995
Matrix30 <Matrixboy30@gmail.com>
2023-11-13 02:16:31 +0100

    see test

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -3,44 +3,57 @@
 Test module for state model
 """
 
+import datetime
 import unittest
 from models.base_model import BaseModel
-from models.state import State
+from models.user import User
 from models.engine.file_storage import FileStorage
 
 
-class TestState(unittest.TestCase):
+class TestUser(unittest.TestCase):
     """Test class for users"""
 
     def test_init(self):
-        state1 = State()
+        user1 = User()
         # test if the object is an instance of the class
         # and not parent class BaseModel
-        self.assertIsInstance(state1, State)
+        self.assertIsInstance(user1, User)
         # test datatype for all attributes
-        state1.name = "Bamenda"
-        self.assertIsInstance(state1.name, str)
+        self.assertIsInstance(user1.id, str)
+        user1.first_name = "Bamenda"
+        self.assertIsInstance(user1.first_name, str)
+        user1.last_name = "Bamenda"
+        self.assertIsInstance(user1.last_name, str)
+        user1.email = "airbnb@gmail.com"
+        self.assertIsInstance(user1.email, str)
+        user1.password = "root"
+        self.assertIsInstance(user1.password, str)
 
         # test for unique id's for users
-        s1 = State()
-        s2 = State()
-        self.assertNotEqual(s1.id, s2.id)
+        u1 = User()
+        u2 = User()
+        self.assertNotEqual(u1.id, u2.id)
         # test for different creation time
-        self.assertNotEqual(s1.created_at, s2.created_at)
+        self.assertNotEqual(u1.created_at, u2.created_at)
 
         # test for different update time
-        self.assertNotEqual(s1.updated_at, s2.updated_at)
+        self.assertNotEqual(u1.updated_at, u2.updated_at)
 
         # testing default attributes values for user
-        self.assertEqual(s1.name, "")
+        self.assertIsInstance(user1.id, str)
+        self.assertIsInstance(user1.created_at, datetime)
+        self.assertIsInstance(user1.first_name, str)
+        self.assertIsInstance(user1.last_name, str)
+        self.assertIsInstance(user1.email, str)
+        self.assertIsInstance(user1.password, str)
 
         # test setting attributes
-        s1.name = "Bamenda"
-        self.assertEqual(s1.name, "Bamenda")
+        u1.password = "hack"
+        self.assertEqual(u1.password, "hack")
 
         # test if user attribute can be created from kwarg
-        s1 = State(country="Cameroon")
-        self.assertEqual(s1.country, "Cameroon")
+        u1 = User(country="Cameroon")
+        self.assertEqual(u1.country, "Cameroon")
 
 
 if __name__ == "__main__":

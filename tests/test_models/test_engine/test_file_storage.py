03eb051123862d088cddb10bb027cfa04f27d1a8
Matrix30 <Matrixboy30@gmail.com>
2023-11-13 02:45:08 +0100

    finish file storage test

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,31 +1,21 @@
 #!/usr/bin/python3
-"""Defines unittests for models/engine/file_storage.py.
-Unittest classes:
-    TestFileStorage_instantiation
-    TestFileStorage_methods
+"""Test module for file storage class
 """
 import os
-import pep8
-import json
 import models
 import unittest
-from datetime import datetime
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
 from models.user import User
-from models.state import State
-from models.place import Place
-from models.city import City
-from models.amenity import Amenity
-from models.review import Review
 
-class TestFileStorage_methods(unittest.TestCase):
-    """Unittests for testing methods of the FileStorage class."""
+
+class TestFileStorage(unittest.TestCase):
+    """Test class for file storage"""
 
     @classmethod
     def setUp(self):
         try:
-            os.rename("file.json", "tmp")
+            os.rename("file.json", "tmp_json")
         except IOError:
             pass
 
@@ -36,11 +26,10 @@ class TestFileStorage_methods(unittest.TestCase):
         except IOError:
             pass
         try:
-            os.rename("tmp", "file.json")
+            os.rename("tmp_json", "file.json")
         except IOError:
             pass
         FileStorage._FileStorage__objects = {}
-
 
     def test_attributes(self):
         self.assertIsInstance(FileStorage.__file_path, str)
@@ -49,15 +38,10 @@ class TestFileStorage_methods(unittest.TestCase):
     def test_all(self):
         self.assertEqual(dict, type(models.storage.all()))
 
-    def test_all_with_arg(self):
-        with self.assertRaises(TypeError):
-            models.storage.all(None)
-
     def test_new(self):
-        bm = BaseModel()
-        models.storage.new(bm)
-        self.assertIn("BaseModel." + bm.id, models.storage.all().keys())
-
+        basemodel = BaseModel()
+        models.storage.new(basemodel)
+        self.assertIn("BaseModel." + basemodel.id, models.storage.all().keys())
 
     def test_new_with_args(self):
         with self.assertRaises(TypeError):
@@ -72,7 +56,7 @@ class TestFileStorage_methods(unittest.TestCase):
         with open("file.json", "r") as file:
             text = file.read()
             self.assertIn("User." + us.id, text)
-        
+
     def test_reload(self):
         """
         Tests method: reload (reloads objects from string file)
@@ -82,15 +66,14 @@ class TestFileStorage_methods(unittest.TestCase):
             os.remove("file.json")
         except FileNotFoundError:
             pass
-        with open("file.json", "w") as f:
-            f.write("{}")
-        with open("file.json", "r") as r:
-            for line in r:
+        with open("file.json", "w") as file:
+            file.write("{}")
+        with open("file.json", "r") as reader:
+            for line in reader:
                 self.assertEqual(line, "{}")
         self.assertIs(a_storage.reload(), None)
 
-
-    def test_reload_with_arg(self):
+    def test_reload(self):
         with self.assertRaises(TypeError):
             models.storage.reload(None)
 
